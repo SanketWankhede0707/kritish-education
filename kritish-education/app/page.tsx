14c2c2faa88010fae4f5ceb54e621056bb8c83eb
Sanket Wankhede <80680306+SanketWankhede0707@users.noreply.github.com>
2025-12-03 10:28:13 +0000

    deleted:    LICENSE 	new file:   kritish-education/.gitignore 	new file:   kritish-education/README.md 	new file:   kritish-education/app/favicon.ico 	new file:   kritish-education/app/globals.css 	new file:   kritish-education/app/layout.tsx 	new file:   kritish-education/app/page.tsx 	new file:   kritish-education/eslint.config.mjs 	new file:   kritish-education/next.config.ts 	new file:   kritish-education/package-lock.json 	new file:   kritish-education/package.json 	new file:   kritish-education/postcss.config.mjs 	new file:   kritish-education/public/file.svg 	new file:   kritish-education/public/globe.svg 	new file:   kritish-education/public/next.svg 	new file:   kritish-education/public/vercel.svg 	new file:   kritish-education/public/window.svg 	new file:   kritish-education/tsconfig.json
     Changes not staged for commit:
    	modified:   kritish-education/app/globals.css
    	modified:   kritish-education/app/page.tsx
    	modified:   kritish-education/package-lock.json
    	modified:   kritish-education/package.json
    
     Untracked files:
    	kritish-education/postcss.config.js
    	kritish-education/public/images/
    	kritish-education/tailwind.config.js

---
+++ kritish-education/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Project Started Successfully
+      </h1>
+      <p className="text-gray-500 mt-4">
+        Tailwind is working if the title is Blue.
+      </p>
+    </main>
+  );
+}
