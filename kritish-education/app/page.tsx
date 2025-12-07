f500d52de168a5c31bc8f2749c9d73db7227ddb8
Sanket Wankhede <80680306+SanketWankhede0707@users.noreply.github.com>
2025-12-07 14:27:44 +0000

    Changes to be committed: 	modified:   kritish-education/app/page.tsx 	modified:   kritish-education/components/Navbar.tsx

--- kritish-education/app/page.tsx
+++ kritish-education/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Hero />
       <Services />
       <Destinations />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Stats />
       <CTA />
     </main>
--- kritish-education/components/Navbar.tsx
+++ kritish-education/components/Navbar.tsx
@@ -34,6 +34,7 @@ export default function Navbar() {
             <li><Link href="/services" onClick={() => setIsMobileOpen(false)}>Services</Link></li>
             <li><Link href="/destinations" onClick={() => setIsMobileOpen(false)}>Destinations</Link></li>
             <li><Link href="/success-stories" onClick={() => setIsMobileOpen(false)}>Success Stories</Link></li>
+            <li><Link href="/" onClick={() => setIsMobileOpen(false)}>Blog's</Link></li>
             <li>
               <button 
                 // Button Logic: Opens the modal
